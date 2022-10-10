a1c527a4173c772413984b5c6baa5119ef2cdf75
Hloij <93326002+Hloij@users.noreply.github.com>
2022-10-10 11:09:56 +0300

    Добавьте файлы проекта.

---
+++ Cha/Cha/Page1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cha
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();          
+            Watch();
+        }
+        bool work=true;
+        DateTime date = DateTime.Now.Date;   
+        public async void Watch()
+        {
+            DateTim.Text = date.ToString("M");
+            while (work==true)
+            {
+                Tim.Text = DateTime.Now.ToString("T");
+                if(date != DateTime.Now.Date)
+                {
+                    date = DateTime.Now.Date;
+                    DateTim.Text = date.ToString("M");
+
+                }
+                await Task.Delay(1000);
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
